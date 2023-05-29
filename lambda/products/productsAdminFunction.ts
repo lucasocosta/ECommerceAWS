c28859a21a986843760e11dea6810cb035015640
Lucas Oliveira Costa <lucasocosta@hotmail.com>
2023-05-29 19:51:50 +0000

    refactor: refatora código para melhor leitura

--- lambda/products/productsAdminFunction.ts
+++ lambda/products/productsAdminFunction.ts
@@ -23,54 +23,74 @@ export async function handler(
   );
 
   if (event.resource === "/products") {
-    const product = JSON.parse(event.body!) as Product;
     console.log("POST /products");
 
-    const productCreated = await productRepository.create(product);
-    return {
-      statusCode: 201,
-      body: JSON.stringify(productCreated),
-    };
+    const product = JSON.parse(event.body!) as Product;
+    const result = await createProduct(product);
+
+    return result;
   } else if (event.resource === "/products/{id}") {
     const productId = event.pathParameters!.id as string;
     console.log(`{${method} /products/${productId}`);
+
     if (method === "PUT") {
       const product = JSON.parse(event.body!) as Product;
-      try {
-        const productUpdated = await productRepository.updateProduct(
-          productId,
-          product
-        );
-        return {
-          statusCode: 200,
-          body: JSON.stringify(productUpdated),
-        };
-      } catch (ConditionalCheckFailedException) {
-        console.error((<Error>ConditionalCheckFailedException).message);
-        return {
-          statusCode: 404,
-          body: JSON.stringify({ message: "Product not found" }),
-        };
-      }
-    } else if (method === "DELETE") {
-      try {
-        const productDeleted = await productRepository.deleteProduct(productId);
-        return {
-          statusCode: 200,
-          body: JSON.stringify(productDeleted),
-        };
-      } catch (err) {
-        console.error((<Error>err).message);
-        return {
-          statusCode: 404,
-          body: JSON.stringify({ message: (<Error>err).message }),
-        };
-      }
+      const result = await updateProduct(productId, product);
+      return result;
     }
+    const result = await deleteProduct(productId);
+    return result;
   }
 
   return {
     statusCode: 400,
     body: JSON.stringify({ message: "Get Products Error" }),
   };
+}
+
+ async function createProduct(product: Product): Promise<APIGatewayProxyResult> {
+  const productCreated = await productRepository.create(product);
+  return {
+    statusCode: 201,
+    body: JSON.stringify(productCreated),
+  };
+}
+
+async function updateProduct(
+  productId: string,
+  product: Product
+): Promise<APIGatewayProxyResult> {
+  try {
+    const productUpdated = await productRepository.updateProduct(
+      productId,
+      product
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(productUpdated),
+    };
+  } catch (ConditionalCheckFailedException) {
+    console.error((<Error>ConditionalCheckFailedException).message);
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: "Product not found" }),
+    };
+  }
+}
+async function deleteProduct(
+  productId: string
+): Promise<APIGatewayProxyResult> {
+  try {
+    const productDeleted = await productRepository.deleteProduct(productId);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(productDeleted),
+    };
+  } catch (err) {
+    console.error((<Error>err).message);
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: (<Error>err).message }),
+    };
+  }
 }
--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -23,31 +23,43 @@ export async function handler(
 
   if (event.resource === "/products") {
     console.log("GET /products");
-    const products = await productRepository.getAllProducts();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(products),
-    };
+    const result = await getAllProducts();
+    
+    return result;
   } else if (event.resource === "/products/{id}") {
     const productId = event.pathParameters!.id as string;
     console.log(`GET /products/${productId}`);
-    try {
-      const product = await productRepository.getProductById(productId);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(product),
-      };
-    } catch (err) {
-      console.error((<Error>err).message);
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ message: (<Error>err).message }),
-      };
-    }
+    const result = await getProductById(productId);
+
+    return result;
   }
 
   return {
     statusCode: 400,
     body: JSON.stringify({ message: "Error getting product" }),
   };
+}
+
+ async function getAllProducts(): Promise<APIGatewayProxyResult> {
+  const products = await productRepository.getAllProducts();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products),
+  };
+}
+
+async function getProductById(productId: string): Promise<APIGatewayProxyResult> {
+  try {
+    const product = await productRepository.getProductById(productId);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(product),
+    };
+  } catch (err) {
+    console.error((<Error>err).message);
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: (<Error>err).message }),
+    };
+  }
 }
